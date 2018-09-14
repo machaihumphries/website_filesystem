6e7e0563875c7e398dd93522e04223dfd230dc8a
Machai-Humphries <first.last@hgs.hiddengeniusproject.org>
2018-09-13 19:02:45 -0700

    first commit

---
+++ wedsitefilesystem.sh
@@ -0,0 +1,24 @@
+pwd				#print	working	directory
+cd ~/desktop				#make
+mkdir website_filesystem				#making website_filesystem	folder
+cd website_filesystem				# making_website_filsystem	file
+touch index.html				#making_index.html_file
+mkdir html css js img extra				#makeing_html css js img extra_folders
+cd html 				#
+touch pg1.html 				#goes into pg1.html 
+touch pg2.html 				#goes into pg2.html 
+cd ../css 				#moves to css file
+touch pg1.css 				#goes into pg.1css
+touch pg2.css 				#goes into mg.2css
+touch index.css 				#goes into index.css files
+cd ../js 				#moves to js
+touch pg1.js 				#goes into pg1.js
+touch pg2.js 				#goes into pg.js
+touch index.js 				#goes into index.js
+cd ../extra 				#moves to extra file
+mkdir audio 				#goes into audio folders
+mkdir video 				#goes into audio folders
+cd ../img
+curl -o lion.jpg "http://historyofarmenia.org/wp-content/uploads/2017/06/maxresdefault.jpg" 												#downloding and making image
+curl -o zebra.jpg "http://animals.sandiegozoo.org/sites/default/files/2016-08/hero_zebra_animals.jpg"										#downloding and making image
+curl -o scropian.jpg "https://res.cloudinary.com/dk-find-out/image/upload/q_80,w_1920,f_auto/DCTM_Penguin_UK_DK_AL652398_in2yky.jpg"		#downloding and making image
